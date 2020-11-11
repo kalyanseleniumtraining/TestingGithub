13910ef820573b88f771efa709e3cbbb3e994beb
kalya <kalya@192.168.0.8>
2020-11-11 09:51:34 +0530

    Test

---
+++ FirstMavenProject/src/test/java/testcases/TestCase1.java
@@ -0,0 +1,14 @@
+package testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+
+	@Test
+	public void ts01() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://naukri.com");
+	}
+}
